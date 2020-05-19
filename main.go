b889a9b5fb0e466662a185c70ed1bd15154a3d46
kn <kn@gmail.com>
2020-05-20 07:52:09 +0800

    补全目录

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/kataras/iris"
+
+func main() {
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+}
