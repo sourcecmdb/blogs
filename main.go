4fa6c418601e8dde3252d92171199f4a44c5bb26
kn <kn@gmail.com>
2020-05-20 08:28:06 +0800

    demo

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"github.com/kataras/iris"
+)
 
 func main() {
 	app := iris.New()
 	app.Logger().SetLevel("debug")
+	app.Get("/", func(context iris.Context) {
+context.HTML("<h1>Hello World</h1>")
+	})
 
 	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
