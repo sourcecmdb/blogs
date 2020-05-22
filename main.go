526fdec8c137ab9205e367064a903591e5d17255
kn <kn@gmail.com>
2020-05-22 10:37:37 +0800

    编写入口文件

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	app:= gin.Default()
+	app := gin.Default()
 	app.Use(gin.Logger())
+
+	app.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
 	app.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
