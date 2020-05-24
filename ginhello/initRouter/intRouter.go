aa4396178722682221ca68afabad23e35eb82a30
access <kevinlandun@gmail.com>
2020-05-24 15:54:59 +0800

    新增 静态文件目录配置

--- ginhello/initRouter/intRouter.go
+++ ginhello/initRouter/intRouter.go
@@ -2,10 +2,9 @@ package initRouter
 
 import (
 	"fmt"
-	"github.com/sourcecmdb/blogs/ginhello/Handler"
-	"net/http"
-	"strings"
 	"time"
+
+	"github.com/sourcecmdb/blogs/ginhello/Handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +12,13 @@ import (
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(gin.Logger())
+	if mode := gin.Mode(); mode == gin.TestMode {
+		router.LoadHTMLGlob("./../templates/*")
+	} else {
+		router.LoadHTMLGlob("templates/*")
+	}
+	router.StaticFile("/favicon.ico", "favicon.ico")
+	router.Static("/static", "./static")
 
 	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
 
@@ -33,25 +39,32 @@ func SetupRouter() *gin.Engine {
 	// 	context.String(http.StatusOK, "GIN hello world")
 	// })
 
+	//index := router.Group("/")
+	//{
+	//	//// 添加 Get 请求路由
+	//	//index.GET("", retHelloGinAndMethod)
+	//	//// 添加 Post 请求路由
+	//	//index.POST("", retHelloGinAndMethod)
+	//	//// 添加 Put 请求路由
+	//	//index.PUT("", retHelloGinAndMethod)
+	//	//// 添加 Delete 请求路由
+	//	//index.DELETE("", retHelloGinAndMethod)
+	//	//// 添加 Patch 请求路由
+	//	//index.PATCH("", retHelloGinAndMethod)
+	//	//// 添加 Head 请求路由
+	//	//index.HEAD("", retHelloGinAndMethod)
+	//	//// 添加 Options 请求路由
+	//	//index.OPTIONS("", retHelloGinAndMethod)
+	//	// 万能方法
+	//	//index.Any("",retHelloGinAndMethod)
+	//	index.Any("",Handler.Index)
+	//}
 	index := router.Group("/")
 	{
-		//// 添加 Get 请求路由
-		//index.GET("", retHelloGinAndMethod)
-		//// 添加 Post 请求路由
-		//index.POST("", retHelloGinAndMethod)
-		//// 添加 Put 请求路由
-		//index.PUT("", retHelloGinAndMethod)
-		//// 添加 Delete 请求路由
-		//index.DELETE("", retHelloGinAndMethod)
-		//// 添加 Patch 请求路由
-		//index.PATCH("", retHelloGinAndMethod)
-		//// 添加 Head 请求路由
-		//index.HEAD("", retHelloGinAndMethod)
-		//// 添加 Options 请求路由
-		//index.OPTIONS("", retHelloGinAndMethod)
 		// 万能方法
-		index.Any("",retHelloGinAndMethod)
+		index.Any("", Handler.Index)
 	}
+
 	UserRouter := router.Group("/user")
 	{
 		UserRouter.GET("/:name", Handler.SaveUser)
@@ -60,6 +73,6 @@ func SetupRouter() *gin.Engine {
 	return router
 }
 
-func retHelloGinAndMethod(c *gin.Context) {
-	c.String(http.StatusOK,"hello gin"+strings.ToLower(c.Request.Method)+"method")
-}
+//func retHelloGinAndMethod(c *gin.Context) {
+//	c.String(http.StatusOK,"hello gin"+strings.ToLower(c.Request.Method)+"method")
+//}
--- test/index_test.go
+++ test/index_test.go
@@ -2,15 +2,23 @@ package test
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/sourcecmdb/blogs/ginhello/initRouter"
 	"net/http"
 	"net/http/httptest"
-	_"strconv"
+	_ "strconv"
 	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sourcecmdb/blogs/ginhello/initRouter"
 
 	"github.com/stretchr/testify/assert"
 )
+
+var router *gin.Engine
+
+func init() {
+	gin.SetMode(gin.TestMode)
+	router = initRouter.SetupRouter()
+}
 
 // TestMain =函数可以在测试函数执行之前做一些其他的操作
 func TestMain(m *testing.M) {
@@ -20,16 +28,16 @@ func TestMain(m *testing.M) {
 }
 
 func TestAll(t *testing.T) {
-	gin.SetMode(gin.TestMode)
+	//gin.SetMode(gin.TestMode)
 	fmt.Println("开始测试 Router 中的相关方法")
 	//t.Run("测试获取Router所有用户", testIndexGetRouter)
 	//t.Run("测试获取Router所有用户", testUserSave)
-	t.Run("测试获取Router所有用户", testUserSaveQuery)
+	// t.Run("测试获取Router所有用户", testUserSaveQuery)
+	t.Run("测试获取Router所有用户", testIndexHtml)
 }
 
-
 func testIndexGetRouter(t *testing.T) {
-	router := initRouter.SetupRouter()
+	//router := initRouter.SetupRouter()
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodGet, "/", nil)
 	router.ServeHTTP(w, req)
@@ -40,7 +48,7 @@ func testIndexGetRouter(t *testing.T) {
 
 func testUserSave(t *testing.T) {
 	username := "lisi"
-	router := initRouter.SetupRouter()
+	//router := initRouter.SetupRouter()
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodGet, "/user/"+username, nil)
 	router.ServeHTTP(w, req)
@@ -51,7 +59,7 @@ func testUserSave(t *testing.T) {
 func testUserSaveQuery(t *testing.T) {
 	username := "lisi"
 	//age := 18
-	router := initRouter.SetupRouter()
+	//router := initRouter.SetupRouter()
 	w := httptest.NewRecorder()
 	//req, _ := http.NewRequest(http.MethodGet, "/user?name="+username+"&age="+strconv.Itoa(age), nil)
 	req, _ := http.NewRequest(http.MethodGet, "/user?name="+username, nil)
@@ -59,4 +67,13 @@ func testUserSaveQuery(t *testing.T) {
 	assert.Equal(t, http.StatusOK, w.Code)
 	//assert.Equal(t, "用户:"+username+",年龄:"+strconv.Itoa(age)+"已经保存", w.Body.String())
 	assert.Equal(t, "用户:"+username+",年龄:20已经保存", w.Body.String())
+}
+
+func testIndexHtml(t *testing.T) {
+	//router := initRouter.SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), "hello gingetmethod")
 }
