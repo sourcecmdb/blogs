d8437a537f144a119dd8a80385a12e2083a58fa6
access <kevinlandun@gmail.com>
2020-05-30 17:06:08 +0800

    日志文件配置

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/sourcecmdb/blogs/initRouter"
 )
 
@@ -10,6 +9,6 @@ func main() {
 	router := initRouter.SetupRouter()
 
 	//gin.SetMode(gin.DebugMode) // debug模式
-	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
 	_ = router.Run(":8080")
 }
--- middleware/Logger.go
+++ middleware/Logger.go
@@ -10,10 +10,11 @@ import (
 func Logger() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		host := context.Request.Host
+		client := context.Request.RemoteAddr
 		url := context.Request.URL
 		method := context.Request.Method
-		fmt.Printf("\n %s::%s \t %s \t %s ", time.Now().Format("2006-01-02 15:04:05"), host, url, method)
+		fmt.Printf("%s::%s \t %s \t %s \t %s  ", time.Now().Format("2006-01-02 15:04:05"), host, url, client, method)
 		context.Next()
-		fmt.Println(context.Writer.Status())
+		//fmt.Println(context.Writer.Status())
 	}
 }
