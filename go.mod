79f91558a0ae97d3be3ccffa3e18a8b7c577c0c3
kn <kn@gmail.com>
2020-05-31 16:46:33 +0800

    更新gorm

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.12
 	github.com/stretchr/testify v1.6.0
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
 	github.com/swaggo/gin-swagger v1.2.0
--- initDB/initDB.go
+++ initDB/initDB.go
@@ -3,17 +3,18 @@ package initDB
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"log"
 )
 
-var Db *sql.DB
+var Db *gorm.DB
 
 func init() {
 	var err error
-	Db, err = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/ginhello")
+	Db, err = gorm.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/ginhello")
 	if err != nil {
 		log.Panicln("err:", err.Error())
 	}
-	Db.SetMaxOpenConns(10)
-	Db.SetMaxIdleConns(10)
+	//Db.SetMaxOpenConns(10)
+	//Db.SetMaxIdleConns(10)
 }
